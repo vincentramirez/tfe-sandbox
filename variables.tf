cfc5a71977b66eb808ddf23c919656912b1a1cc3
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-02-15 22:22:21 -0600

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {}
+
+variable "ami" {}
+
+variable "environment_name" {}
+
+variable "instance_type" {}
+
+variable "ssh_key" {}
+
+variable "owner" {}
+
+variable "ttl" {}
+
+  
+
+
