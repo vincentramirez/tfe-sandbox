e7fd830c2286f711d8dd0b84020ed6ee9c6e74a9
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-03-26 08:07:56 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_dns" {
-  value = "${aws_instance.ec2_vm.public_dns}"
+  value = "${aws_instance.ec2_vm.*.public_dns}"
 }
 
 output "instance_public_ip" {
-  value = "${aws_instance.ec2_vm.public_ip}"
+  value = "${aws_instance.ec2_vm.*.public_ip}"
 }
