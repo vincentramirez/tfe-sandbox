33bfb3da96a715ed36c35004e4eaff36771d7786
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-03-26 08:00:00 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -7,11 +7,11 @@ resource "aws_instance" "ec2_vm" {
   count = "${var.count}"
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
-  root_block_device {
+  #root_block_device {
   #volume_type = "gp2"
-  volume_size = "${var.instance_size}"
-  delete_on_termination = "true"
-  } 
+  #volume_size = "${var.instance_size}"
+  #delete_on_termination = "true"
+  #} 
   # the VPC subnet
   subnet_id = "${aws_subnet.subnet-public-1a.id}"
 
